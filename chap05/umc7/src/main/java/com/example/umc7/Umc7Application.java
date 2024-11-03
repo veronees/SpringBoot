c919ecab1abac959310742457ca2b2ae382a98e6
veronees <jsg6854@naver.com>
2024-11-03 16:53:45 +0900

    BaseTime 적용

--- chap05/umc7/src/main/java/com/example/umc7/Umc7Application.java
+++ chap05/umc7/src/main/java/com/example/umc7/Umc7Application.java
@@ -2,7 +2,9 @@ package com.example.umc7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Umc7Application {
 
